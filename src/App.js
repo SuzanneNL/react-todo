b9355c8999d056bddbbff8ac86c9f4f1b4303661
suzannenl <skr470@gmail.com>
2022-03-23 17:45:00 +0100

    Use router to switch between components for list of todos, add todo and edit todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import EditTodo from "./components/AddTodo";
+
 function App() {
   return (
     <div className="App">
-      <h1>To do</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Todos} />
+          <Route path="/add" component={AddTodo} />
+          <Route path="/edit/:id" component={EditTodo} />
+        </Switch>
+      </Router>
     </div>
   );
 }
