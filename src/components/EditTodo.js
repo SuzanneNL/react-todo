caffd9cd4a9e932f64ac263cc52215ef9dbb3e09
suzannenl <skr470@gmail.com>
2022-03-25 15:20:27 +0100

    Add form for editing todos

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Card from './layout/Card'
 
 const EditTodo = () => {
   return (
-    <div>EditTodo</div>
+    <Card>
+      <form>
+        <div class="form-group">
+          <label for="todo">Edit todo</label>
+          <input
+            type="text"
+            class="form-control"
+            id="todo"
+            placeholder="Enter your todo"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Edit todo
+        </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
+      </form>
+    </Card>
   )
 }
 
