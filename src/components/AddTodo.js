9f0e980d6df57a5c03e252249574fa9e91b3245a
suzannenl <skr470@gmail.com>
2022-03-25 15:19:45 +0100

    Add form for adding todos

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Card from "./layout/Card";
 
 const AddTodo = () => {
   return (
-    <div>AddTodo</div>
-  )
-}
+    <Card>
+      <form>
+        <div class="form-group">
+          <label for="todo">Add todo</label>
+          <input
+            type="text"
+            class="form-control"
+            id="todo"
+            placeholder="Enter your todo"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
+      </form>
+    </Card>
+  );
+};
 
-export default AddTodo
+export default AddTodo;
